68512ba07ea80b90d2b90a535ce7c1bade6e5af0
Fab_ <30447649+Fabio-Git-dev@users.noreply.github.com>
2020-09-22 13:06:53 +0200

    Update main.c

--- Numbers Loop/main.c
+++ Numbers Loop/main.c
@@ -3,19 +3,21 @@
 
 int main() {
 
-int x, y, z; //variables
+        int x, y, z; //variables
 
-while (1) {
+    
+        while (1) {
 
-x = 0; //assignment of values to variables
-y = 1;
+              x = 0; //assignment of values to variables
+     
+              y = 1;
 
-do {
-printf("%d\n", x); //result output at end line
+     do {
+            printf("%d\n", x); //result output at end line
 
-z = x + y; //Z attribute
-x = y;
-y = z;
+             z = x + y; //Z attribute
+             x = y;
+             y = z;
 
      }    while (x < 255); //In this case x must be lower than 255
 
