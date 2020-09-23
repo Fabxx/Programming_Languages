64e09bc57e6cf7ed3ad694005a33f46c9fcaaa36
Fab_ <30447649+Fabio-Git-dev@users.noreply.github.com>
2020-09-23 11:27:08 +0200

    Update main.c

--- 4 -Operations with numbers/Sum in printf/main.c
+++ 4 -Operations with numbers/Sum in printf/main.c
@@ -4,10 +4,10 @@
 int main()
 
  {
-    int number1 = 10;
+    int number1 = 10; //Variables
     int number2 = 25;
     int number3 = 30;
 
-     printf("%d", number1 + number2 +  number3);
+     printf("%d", number1 + number2 +  number3); //Sum output in printf
 
     }
