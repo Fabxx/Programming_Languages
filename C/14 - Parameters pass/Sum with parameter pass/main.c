ca3be6b5f9101a57a8d9b59054913d23348beea2
Fab_ <30447649+Fabio-Git-dev@users.noreply.github.com>
2021-03-22 17:43:18 +0100

    Add files via upload

---
+++ C/14 - Parameters pass/Sum with parameter pass/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 2
+
+void calcs (int numbers[MAX]); //declaration of void function with the parameters
+
+int main(void) { //main function
+
+     int n[MAX]; //array used to pass the values to numbers.
+
+
+    calcs(n); //calling calcs funciton with n array parameter, so the values that will go in n array will pass in numbers array.
+
+    system("pause");
+
+    return 0;
+}
+
+
+void calcs(int numbers[MAX]) {
+
+    int i;
+    int sum;
+
+        for (i = 0; i < MAX; i++) {
+
+            printf("Insert the numbers\n");
+            scanf("%d", &numbers[i]);
+
+        }
+
+
+            for (i = 0; i < MAX; i++) {
+
+                sum = sum + numbers[i];
+
+            }
+
+                printf("Sum:%d\n", sum);
+}
