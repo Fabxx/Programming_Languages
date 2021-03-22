2f38eaecfbf1fe61db20770a350857e2e22a98a1
Fab_ <30447649+Fabio-Git-dev@users.noreply.github.com>
2021-03-22 15:12:57 +0100

    Update the type void.c
    Adjusted indentation.

--- 9 - functions/the type void.c
+++ 9 - functions/the type void.c
@@ -12,11 +12,19 @@ piersilvio spicoli - uniba informatica
 */
 
 //ddefinition of void
-void square(int a){ printf("%d", a*a); } //in this case we pass a parameter
+void square(int a) { 
+	
+	printf("%d", a*a); //in this case we pass a parameter
 
-void printMsg(){ printf("Hello World! I'm here!"); } //in this instead we simply print a message
+} 
 
-int main(){
+void printMsg() { 
+	
+	printf("Hello World! I'm here!"); //in this instead we simply print a message
+
+} 
+
+int main() {
 	
 	int x = 5;
 	square(x);
