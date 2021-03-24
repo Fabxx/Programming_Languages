2f0992661ea77878174d87a4b3fe15751800c2b2
Fab_ <30447649+Fabio-Git-dev@users.noreply.github.com>
2021-03-24 19:41:26 +0100

    Add files via upload

---
+++ Python/3 - Simple if - else usage/main.py
@@ -0,0 +1,11 @@
+
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+
+sum = a + b
+if(sum > 40):
+
+    print("The sum is over 40:", sum)
+else:
+
+    print("The sum is under 40:", sum)
