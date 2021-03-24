b5c3124e76d96eda9a1a9cdec26a97afa3d56dbc
Fab_ <30447649+Fabio-Git-dev@users.noreply.github.com>
2021-03-24 19:26:16 +0100

    Add files via upload

---
+++ Python/1 - Usage of variables and Printf/main.py
@@ -0,0 +1,7 @@
+# This is a sample Python script.
+
+# Press Maiusc+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+print("Hello world!");
