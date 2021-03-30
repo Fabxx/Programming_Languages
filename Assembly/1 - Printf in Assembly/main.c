b6d2785998705a337872ff72eafed755701b16fe
Fab_ <30447649+Fabio-Git-dev@users.noreply.github.com>
2021-03-30 13:13:05 +0200

    Add files via upload

---
+++ Assembly/1 - Printf in Assembly/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Simple sum in ASM assembly*/
+
+int main()
+{
+        int foo = 10, bar = 15; //variables.
+
+    asm volatile("addl  %%ebx,%%eax" //function in assembler to sum.
+
+                             :"=a"(foo) //we assign to a the foo value, to b the bar value.
+
+                             :"a"(foo), "b"(bar)
+
+                             ); //end of ASM.
+
+        printf("foo + bar = %d\n", foo); //we print the final value of foo.
+        return 0;
+}
