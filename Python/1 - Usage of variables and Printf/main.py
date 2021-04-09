5a72708a8a4c976ce29877b8e27d5eb53a376cec
Fab_ <30447649+Fabio-Git-dev@users.noreply.github.com>
2021-04-09 11:14:11 +0200

    Update main.py

--- Python/1 - Usage of variables and Printf/main.py
+++ Python/1 - Usage of variables and Printf/main.py
@@ -1,4 +1,4 @@
-# This is a sample Python script.
+# This is a sample Python script For Beginners.
 
 # Press Maiusc+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
