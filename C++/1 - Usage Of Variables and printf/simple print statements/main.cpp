8db7e081b9d5bbbb0b8192765ec3b3b435078d93
darawoo <darawoo@umich.edu>
2021-04-11 17:58:12 -0400

    Variables, arithmetic, and if else

---
+++ C++/1 - Usage Of Variables and printf/simple print statements/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+// NOTE: To run these programs, open terminal, go into the correct directory
+// ex: C++/1 - Usage of Variables and printf/complex print statements with variables
+// Run "g++ -o main main.cpp" and then "./main"
+
+int main()
+{
+    // prints "My dad's name is Bob. He is 50 years old. 
+    // His first name starts with the letter B. For lunch,
+    // he had a hamburger for $12.5."
+    cout << "My dad's name is Bob. He is 50 years old. "
+        "His first name starts with the letter B. "
+        "For lunch, he had a hamburger for $12.50." << endl;
+
+    return 0;
+}
