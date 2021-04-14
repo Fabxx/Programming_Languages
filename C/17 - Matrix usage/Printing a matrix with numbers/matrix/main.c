0b061aff01b72b98705ca608d60d4057f7dd805b
Fab_ <30447649+Fabio-Git-dev@users.noreply.github.com>
2021-04-14 21:34:08 +0200

    Update main.c

--- C/17 - Matrix usage/Printing a matrix with numbers/matrix/main.c
+++ C/17 - Matrix usage/Printing a matrix with numbers/matrix/main.c
@@ -5,7 +5,7 @@
 #define ROW 2
 #define COLUMN 2
 
-
+/*This program shows how to print a matrix of numbers in C, by putting numbers*/
 void input();
 
 void input() {
@@ -16,13 +16,14 @@ void input() {
     int i, j;
 
         /*We cicle for both rows and columns, we ask to insert a number for each position in the matrix, the first number will be in the position 1x1
-            the second number in the posiiton 1x2, and so on*/
+            the second number in the posiiton 1x2, where 1 is the row, 2 is the number of the columns and so on*/
 
         for (i = 0; i < ROW; i++) {
 
             printf("Insert the number for the %d row.\n", i+1);
 
             for (j = 0; j < COLUMN; j++) {
+
 
                 scanf("%d", &matrix[i][j]);
 
@@ -32,7 +33,9 @@ void input() {
         /*We do again the for cicle to print the matrix by following the size of it, the numbers will be printed in orders of position, 1x1, 1x2 ecc.
             the last printf helps us to distinguish the rows and the columns by going at the end of the line after the first row is printed.*/
 
+
         for (i = 0; i < ROW; i++) {
+
             for (j = 0; j < COLUMN; j++) {
 
                  printf("%d\t", matrix[i][j]);
@@ -45,3 +48,4 @@ int main(void) {
 
     input();
 }
+
