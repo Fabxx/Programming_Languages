5966743378639ad9f16db06177d79c2d04a5665d
sqr <30447649+Sqrdelta@users.noreply.github.com>
2021-04-18 11:57:38 +0200

    Update main.cpp

--- C++/5- While loops/simple example/main.cpp
+++ C++/5- While loops/simple example/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     int i = 0;
-    // prints the numbers 0-9 (i.e. 10 numbers)
+    // prints the numbers 0-9 (i.e. 10 numbers).
     while (i != 10){
         cout << i << endl;
         i += 1;
