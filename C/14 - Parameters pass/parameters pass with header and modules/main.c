ffa48d81383d8d2982df9db60266b2eb7aee27e3
sqr <30447649+Sqrdelta@users.noreply.github.com>
2021-04-21 19:59:16 +0200

    Add files via upload

---
+++ C/14 - Parameters pass/parameters pass with header and modules/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "math.h"
+#include <time.h>
+
+int main() {
+
+    / * Using the passed parameters, we carry out the operations. Note, even though a and b were declared in the other source's ints, there are no conflicts
+       with other source files. The program randomizes values, with the randomized values we carry out the operations given as an example to the various functions
+       of the other source. This is what the other c file contains, examples to be applied on the parameters that we will pass on the round brackets, nothing more * /
+
+    srand(time(NULL));
+
+    int a = rand() % 10 + 1;
+    int b = rand() % 10 + 1;
+
+    / * Once the values have been randomized, we work on them, using the functions with the necessary operation * /
+
+    int c = power(a);
+
+    int d = power(a) * b;
+
+
+    int e = cube(b);
+
+    int tot = a + e;
+
+    printf("%d\n", tot);
+
+    verify(tot);
+
+
+    return 0;
+}
+
---
+++ C/14 - Parameters pass/parameters pass with header and modules/math.h
@@ -0,0 +1,15 @@
+#ifndef MATH_H_INCLUDED
+#define MATH_H_INCLUDED
+
+/*Qui dichiariamo i prototipi di funzione su cui scriveremo le istruzioni, in questo caso si e' utilizzato un passaggio di parametri*/
+
+int sum(int a, int b);
+int subtraction(int a, int b);
+int multiplication(int a, int b);
+int division(int a, int b);
+int power(int a);
+int cube(int b);
+int verify(int a);
+
+
+#endif // MATH_H_INCLUDED
