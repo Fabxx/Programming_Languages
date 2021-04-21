20e120c79b324777af795214267d39503baace96
sqr <30447649+Sqrdelta@users.noreply.github.com>
2021-04-21 16:46:02 +0200

    Add files via upload

---
+++ C/19 - Modules and cusotm libraries/Operands with custom modules and libraries/main.c
@@ -0,0 +1,14 @@
+
+/*Here we include our custom library with the functions that we made in the .c file.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "operands.h"
+
+int main() {
+
+    sum();
+    sub();
+
+    return 0;
+}
