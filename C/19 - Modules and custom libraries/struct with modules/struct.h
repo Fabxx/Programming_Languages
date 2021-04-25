1f3b3cc8aa01908bd6dba9fe6c66e59a2d302827
sqr <30447649+Sqrdelta@users.noreply.github.com>
2021-04-25 19:30:24 +0200

    Corrected name directory

---
+++ C/19 - Modules and custom libraries/struct with modules/struct.h
@@ -0,0 +1,16 @@
+#ifndef STRUCT_H_INCLUDED
+#define STRUCT_H_INCLUDED
+
+typedef struct {
+
+    int a;
+    int b;
+
+} structure;
+
+structure stru;
+
+void input();
+
+
+#endif // STRUCT_H_INCLUDED
