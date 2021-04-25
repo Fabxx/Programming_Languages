487a5541d418477cf0cf35da2c5d12b1b30135f7
sqr <30447649+Sqrdelta@users.noreply.github.com>
2021-04-25 19:20:49 +0200

    Add files via upload

---
+++ C/19 - Modules and cusotm libraries/struct with modules/struct.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "struct.h"
+
+void input() {
+
+    scanf("%d", &stru.a);
+    scanf("%d", &stru.b);
+
+    printf("%d \n %d\n", stru.a, stru.b);
+
+
+
+}
