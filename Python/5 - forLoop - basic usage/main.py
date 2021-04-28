3e062ac249eddcce6d2b3e9a1f7a18e68f0f5ed7
kali kali <kali@kali.kali>
2021-04-27 21:56:28 -0400

    Simple for loop

--- Python/5 - forLoop - basic usage/main.py
+++ Python/5 - forLoop - basic usage/main.py
@@ -1,5 +1,6 @@
-/*Simple for loop*/
+"""Simple for loop"""
 
+#This loop takes each item on the list and prints a statement with each item
 genre = ['dubstep', 'jazz', 'pop']
 for x in range(len(genre)):
-	print("I like", genre[i])
+	print("I like", genre[x])
