e9460c667fe288eca1bf88cf4b389ec927df13da
kali kali <kali@kali.kali>
2021-04-27 21:58:08 -0400

    Simple while loop

--- Python/8 - while loop - simple usage/main.py
+++ Python/8 - while loop - simple usage/main.py
@@ -1,5 +1,6 @@
-/*Simple while loop*/
-
+"""  Simple while loop """
+#A while loops through an object until the  condition is no longer met
+#In this example the loop continues until i is no longer less than 9
 
 i = 0
 while i < 9:
