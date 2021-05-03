961ca0d37b8db459307f22dd35859c766a7df4d5
sqr <30447649+Sqrdelta@users.noreply.github.com>
2021-05-03 21:30:57 +0200

    Add files via upload

---
+++ C/20 - pointers/password obfuscator/main.c
@@ -0,0 +1,7 @@
+#include "obfuscator.h"
+int main(void) {
+    char array[LENGHT];
+    password(array);
+    return 0;
+}
+
---
+++ C/20 - pointers/password obfuscator/obfuscator.h
@@ -0,0 +1,11 @@
+#ifndef OBFUSCATOR_H_INCLUDED
+#define OBFUSCATOR_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+#define LENGHT 6
+#define MIN 2
+#define MAX 4
+
+void password(char pass[]);
+
+#endif // OBFUSCATOR_H_INCLUDED
