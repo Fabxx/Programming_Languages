3e11f661c013a1e475b3978c81b0a7c8f62ff19a
sqr <30447649+Sqrdelta@users.noreply.github.com>
2021-05-03 15:14:29 +0200

    Add files via upload

---
+++ C/20 - pointers/pointer to struct/main.c
@@ -0,0 +1,37 @@
+/*This program shows how to access to a structure element with pointers*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+
+    int a;
+    int b;
+
+} structure;
+
+//We recall the structure and decalre a pointer, hwich initially iss etted to NULL.
+
+void input(structure *strptr);
+
+int main(void) {
+
+    int ptr = 0;
+
+   input(&ptr);
+
+    return 0;
+
+}
+
+void input(structure *strptr) {
+
+     printf("Insert first value\n");
+    scanf("%d", &strptr->a);
+
+    printf("Insert second value\n");
+    scanf("%d", &strptr->b);
+
+    printf("A:%d\nB:%d\n", strptr->a, strptr->b);
+
+}
