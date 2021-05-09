fe9ad06d85b5806f76d55f229a0461577d7c27f3
sqr <30447649+Sqrdelta@users.noreply.github.com>
2021-05-09 18:46:49 +0200

    Add files via upload

---
+++ C/21 - Files/database for students/include/funzioni.h
@@ -0,0 +1,11 @@
+#include "../include/librerie.h"
+#include "../include/struct.h"
+
+#ifndef FUNZIONI_H_INCLUDED
+#define FUNZIONI_H_INCLUDED
+
+void makefile();
+void input(studenti s[]);
+void average();
+
+#endif //FUNZIONI_H_INCLUDED
---
+++ C/21 - Files/database for students/include/struct.h
@@ -0,0 +1,13 @@
+#include "../include/librerie.h"
+
+#ifndef STRUCT_H_INCLUDED
+#define STRUCT_H_INCLUDED
+
+typedef struct {
+
+    char nominativo[100];
+    float voto[VOTI];
+} studenti;
+
+
+#endif //STRUCT_H_INCLUDED
