4da3e11b7c8c6a90d3c3608f5037b2c5315e02c3
kali kali <kali@kali.kali>
2021-05-16 12:33:31 -0400

    tuples

---
+++ Python/23 - data manipulation - tuples/main.py
@@ -0,0 +1,19 @@
+""" Tuples are a data type that contains more than one item. Once a tuple has been created the contents cannot be changed """
+
+
+
+tuple = (1,2,3,"four")
+
+print(tuple)
+
+
+#Indexing
+#Indexing allows the programmer to select an item in the tuple
+#Picking the first item in the tuple
+print(tuple[0])
+
+#Selecting the last item in the tuple
+print(tuple[-1])
+
+#Selecting more than one item in the tuple
+print(tuple[1:3])
