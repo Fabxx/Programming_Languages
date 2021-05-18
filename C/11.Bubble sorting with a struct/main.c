e86232424b220d93b95963cf44af477fb6f36deb
sqr <30447649+Sqrdelta@users.noreply.github.com>
2021-05-18 19:33:39 +0200

    Update main.c

--- C/11.Bubble sorting with a struct/main.c
+++ C/11.Bubble sorting with a struct/main.c
@@ -43,42 +43,26 @@
       structure tmp;
 
        for (i = 0; i < p-1; i++) {
-
-
         if (str[i].num > str[i+1].num) { //if the number of the first element is major than the next one, we exchange them
-
-
+           
             tmp = str[i]; //the exchange is done with the structure array directly instead of exchanging each parameter of the label.
-
             str[i] = str[i+1];
-
             str[i+1] = tmp;
-
-
         } //end of exchange.
-
              p -= 1; //after the exchange we decrease the position of the elements which has been exchanged.
 
     } //end of for cicle.
 
-
-
 } //end of funciton.
-
-
 
  void print_sort() {
 
     int i;
 
-
     for (i = 0; i < num_elem; i++) {
 
-
         printf("Name:%s\n", str[i].name);
-
           printf("Surname:%s\n", str[i].surname);
-
             printf("Number:%d\n", str[i].num);
 
     }
@@ -87,13 +71,8 @@
  int main(void) {
   
     input();
-
     bubble_sort();
-
     print_sort();
-
-
     system("pause");
-
     return 0;
 }
