9a3035d3fae819d9a8645641f866783c874a3ed4
sqr <30447649+Sqrdelta@users.noreply.github.com>
2021-06-01 19:32:56 +0200

    Update main.c

--- C/20 - pointers/fixing new line segmentation fault with pointers/src/main.c
+++ C/20 - pointers/fixing new line segmentation fault with pointers/src/main.c
@@ -1,6 +1,6 @@
 //we get something on the pointer string, after the input there might be a new line (\n).
     //We remove the newline after the user input aka \n, this can cause a segmentation fault in the buffer.
-    //Returns 0 if we have success and we don't clean the buffer because it's empty, there's no \n on it.
+    //Returns 0 if we have success and we clean the buffer to avoid memory leftovers, there will be no \n on it.
     //Returns 1 if we didn't found any new line, and we clean the buffer.
     //in C, NULL result points to 0x0, which causes a segmentation fault, if we find a \n
     //we avoid that it goes in 0x0 by cleaning the buffer.
