8f0e535749b36bc7fe61104e4702d950be8ae016
kali kali <kali@kali.kali>
2021-06-03 22:40:31 -0400

    Will proofread the comments

---
+++ Python/26 - file manipulation - reading a file/main.py
@@ -0,0 +1,7 @@
+#Reading a file comes in python comes in handy when dealing files that contain data that will later be manipulated
+
+#to read a file in python
+f = open("numbers.csv", "r")
+print(f.readline())
+
+#Besides csv files, excel, json and other file formats can be read and later manipulated
