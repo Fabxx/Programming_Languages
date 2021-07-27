600c6e8f6f8e8766eb994c7710ed7e72586630a8
x86-Avx <30447649+x86-Avx@users.noreply.github.com>
2021-07-27 17:37:08 +0200

    Update main.c

--- C/13 - Array usage, simple and complex/Comparison between array characters/src/main.c
+++ C/13 - Array usage, simple and complex/Comparison between array characters/src/main.c
@@ -2,9 +2,8 @@
  * @file main.c
  * @author x86_Avx
  * @brief This program shows how to do a comparison between two strings with
- * 		  strcmp function. Be aware that to avoid segmentation faults, the strings
- * 		  MUST be arrays with defined size, or else while checking, the program goes
- * 		  out of it's memory range, resulting in a crash.
+ * 		  strcmp function. Be aware that to avoid segmentation faults, the strings either must have a defined constant size, or 
+ 		  you can pass the strings as parameters to have dynamic allocation without pointers and malloc funciton. 
  * @version 0.1
  * @date 2021-07-22
  * 
@@ -12,22 +11,17 @@
  * 
  */
 
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define SIZE 100
+
+char inputstr(char string[]);
+
 int main()
 {
-	char string1[SIZE];
-	char string2[SIZE];
-
-	fgets(string1, sizeof(string1), stdin);
-	fflush(stdin);
-	fgets(string2, sizeof(string2), stdin);
-	fflush(stdin);
-
+	char string1 = inputstr(string1);
+	char string2 = inputstr(string2);
+	
 	if (strcmp(string1, string2) == 0)
 	{
 		puts("The stirngs are the same\n");
@@ -35,8 +29,15 @@ int main()
 	{
 		puts("the stirngs are different\n");
 	}
-	
-	
+
 
 	return (0);
+}
+
+char inputstr(char string[]) { 
+ 
+    fgets(string, sizeof(string), stdin);
+    fflush(stdin);
+    return string;
+
 }
