6ed41059cfeb8621c156a438a37704bb7b474119
Prox_y <30447649+Prox-y@users.noreply.github.com>
2021-08-26 19:18:20 +0200

    Update main.c

--- C/24 - min-max search algorithm/src/main.c
+++ C/24 - min-max search algorithm/src/main.c
@@ -1,6 +1,6 @@
 /**
  * @file main.c
- * @author Prox_y
+ * @author Prox_y, SSUPI
  * @brief This program shows how to search a minimum value and max value
  * 		  in a array of integers.
  * @version 0.1
@@ -38,10 +38,13 @@ void input(int a[]) {
 
 void search_min(int a[]) {
 
-	int i, min = 0;
+	int i, min = INT_MAX;
+	//min gets the maximum value as possible, so the if condition will be always true
+	//and the assignation will happen, if a[i] is always > than min, the assignation
+	//will never happen, same for the max below.
 
 	for (i = 0; i < ARR; i++) {
-		if (a[i] < min){
+		if (a[i] < min) {
 			min = a[i];
 		}
 	}
@@ -51,7 +54,8 @@ void search_min(int a[]) {
 
 void search_max(int a[]) {
 
-	int i, max = 0;
+	int i, max = INT_MIN;
+	//minimum value to check both positive/negative values.
 
 	for (i = 0; i < ARR; i++) {
 		if (a[i] > max) {
