951ae0b4d327b7146eae9c23f6a5c9ff3de23fa9
Prox_y <30447649+Prox-y@users.noreply.github.com>
2021-08-26 19:22:03 +0200

    Update main.c

--- C/24 - min-max search algorithm/src/main.c
+++ C/24 - min-max search algorithm/src/main.c
@@ -2,7 +2,24 @@
  * @file main.c
  * @author Prox_y, SSUPI
  * @brief This program shows how to search a minimum value and max value
- * 		  in a array of integers.
+ * 		  in a array of integers. For more reference:
+ 			
+		CHAR_BIT     8     Defines the number of bits in a byte.
+		SCHAR_MIN     -128     Defines the minimum value for a signed char.
+		SCHAR_MAX     +127     Defines the maximum value for a signed char.
+		UCHAR_MAX     255     Defines the maximum value for an unsigned char.
+		CHAR_MIN     -128     Defines the minimum value for type char and its value will be equal to SCHAR_MIN if char represents negative values, otherwise zero.
+		CHAR_MAX     +127     Defines the value for type char and its value will be equal to SCHAR_MAX if char represents negative values, otherwise UCHAR_MAX.
+		MB_LEN_MAX     16     Defines the maximum number of bytes in a multi-byte character.
+		SHRT_MIN     -32768     Defines the minimum value for a short int.
+		SHRT_MAX     +32767     Defines the maximum value for a short int.
+		USHRT_MAX     65535     Defines the maximum value for an unsigned short int.
+		INT_MIN     -2147483648     Defines the minimum value for an int.
+		INT_MAX     +2147483647     Defines the maximum value for an int.
+		UINT_MAX     4294967295     Defines the maximum value for an unsigned int.
+		LONG_MIN     -9223372036854775808     Defines the minimum value for a long int.
+		LONG_MAX     +9223372036854775807     Defines the maximum value for a long int.
+		ULONG_MAX     18446744073709551615     Defines the maximum value for an unsigned long int.
  * @version 0.1
  * @date 2021-08-26
  * 
