8b079ceccff8fcd62882c3f7809e514e5069971d
Prox_y <30447649+Prox-y@users.noreply.github.com>
2021-09-04 18:22:21 +0200

    Add files via upload

---
+++ C++/11 - Terminal Games/guess the number C++/src/main.cpp
@@ -0,0 +1,65 @@
+/**
+ * @file main.cpp
+ * @author Prox_y
+ * @brief A simpe game where you have to guess the number, but be careful,
+ * 		  if you're wrong, the number to guess will be randomized!
+ * @version 0.1
+ * @date 2021-09-04
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <iostream>
+#include <time.h>
+#define N 5
+
+using namespace std;
+
+void delay(int number_of_seconds)
+{
+    // Converting time into milli_seconds
+    int milli_seconds = 1000 * number_of_seconds;
+  
+    // Storing start time
+    clock_t start_time = clock();
+  
+    // looping till required time is not achieved
+    while (clock() < start_time + milli_seconds);
+}
+  
+// Driver code to test above function
+
+int main() {
+
+	int n = 0, a = 0;
+
+	while (n == 0) {
+		do{
+			int seed = time(NULL);
+			srand(seed);
+
+			n = rand() % (10 - 1 - 0) + 1; //max - 1 - min + min.
+										   //range between 0-10.
+
+			cout << "Guess the number between 1-10!" << endl;
+			cin >> a; 
+			
+			cout << "You failed, try again!" << endl;
+			cout << "Randomizing value in 5 seconds\n" << endl;
+			
+			for (size_t i = 0; i < N; i++) {
+				delay(1); //integer number that will be multiplied to achieve seconds.
+				cout << ".";
+			}
+				system("cls");
+
+		} while (a != n);
+
+			cout << "Success! exiting program...\n" << endl;
+			system("pause");
+			return 0;
+		
+	}
+
+}
