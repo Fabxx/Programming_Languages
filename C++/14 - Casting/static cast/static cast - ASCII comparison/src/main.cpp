8a0647e7885452f267087adbe9b5742ee2f618b1
Prox_y <30447649+Prox-y@users.noreply.github.com>
2021-10-06 20:57:28 +0200

    Update main.cpp

--- C++/14 - Casting/static cast/static cast - ASCII comparison/src/main.cpp
+++ C++/14 - Casting/static cast/static cast - ASCII comparison/src/main.cpp
@@ -6,7 +6,7 @@
  * 	static_cast` is used for cases where you basically want to reverse an implicit conversion, 
  * 	with a few restrictions and additions. `static_cast` performs no runtime checks. 
  * This should be used if you know that you refer to an object of a specific type, 
- * and thus a check would be unnecessary. NOTE: casting from char to int takes ASCII table as reference: https://www.asciitable.com/
+ * and thus a check would be unnecessary. NOTE: casting to char takes ASCII table as reference: https://www.asciitable.com/
  * 		  
     dynamic_cast only works for pointers or references to polymorphic class types;
     const_cast can't change types, only const or volatile qualifiers;
@@ -26,8 +26,8 @@ using namespace std;
 
 int main() {
 
-    char string; 
-    int x;
+    char string; //insert 7 to get the matching
+    int x; //55 it's 7 in ASCII
     cout << "Insert a character, you can insert a single number or symbol" << endl;
     cin >> string;
     cout << "Insert a number" << endl;
@@ -38,8 +38,8 @@ int main() {
             } else{
             cout << "ASCII code is different: " << static_cast<int>(string) << endl << 
                     "Character inserted: " << string << endl 
-                    <<"Integer inserted: \n" << x << endl 
-                    << "ASCII value of integer: \n" << static_cast<char>(x) << endl;
+                    <<"Integer inserted: " << x <<  endl
+                    << "ASCII value of integer: " << static_cast<char>(x) << endl;
         }
         return 0;
 }
