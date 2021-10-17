ff1abce16c3739b42b48e02493477ac75608ca96
Prox_y <30447649+Prox-y@users.noreply.github.com>
2021-10-17 21:26:28 +0200

    Update main.cpp

--- C++/18 - Sounds/Playing wav file/src/main.cpp
+++ C++/18 - Sounds/Playing wav file/src/main.cpp
@@ -3,7 +3,7 @@
  * @author Prox_y
  * @brief This example shows how to play wav files with windows.h 
  *   	  playsound function. NOTE: to be able to reproduce sounds, you need to link
- *        to your compiler, in this case, in the MAKEFILE, the librari winmm.lib
+ *        to your compiler, in this case, in the MAKEFILE, the library winmm.lib
  *  	  into LFLAGS = -lwinmm section.
  * 			
  * @version 0.1
