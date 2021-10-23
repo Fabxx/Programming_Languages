79f0520f91aa5ca1a385613330d553a17fb0ff83
Prox_y <30447649+Prox-y@users.noreply.github.com>
2021-10-23 15:21:25 +0200

    Add files via upload

---
+++ C++/12 - Object Oriented Programming/classes in .h and .cpp/include/libs.h
@@ -0,0 +1,8 @@
+#ifndef C4A58BCD_8817_476A_89EF_FC834873242D
+#define C4A58BCD_8817_476A_89EF_FC834873242D
+#include <iostream>
+using namespace std;
+#include <class.h>
+
+
+#endif /* C4A58BCD_8817_476A_89EF_FC834873242D */
