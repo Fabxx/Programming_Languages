81794b40828ac9e751fd4db2f110b590a6a9d39f
Prox_y <30447649+Prox-y@users.noreply.github.com>
2021-10-26 22:50:20 +0200

    Add files via upload

---
+++ C++/16 - Vectors/Built-in Delete function/src/main.cpp
@@ -0,0 +1,43 @@
+/**
+ * @file main.cpp
+ * @author Prox_Y
+ * @brief Showing how to empty a vector with clear function.
+ * @version 0.1
+ * @date 2021-10-26
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> v;
+
+    int a;
+    for ( size_t i = 0; i < 4; i++ ){
+        cin >> a; 
+        v.push_back(a);
+    }
+
+    cout << "Numbers before: ";
+    for ( int e : v  ) { //scanning vector with e integer.
+        cout << e << ' ';
+    }
+
+    v.clear();
+    
+    cout << "... Numbers After: ";
+    for ( int e : v ) {
+        cout << e << ' ';
+    }
+
+	//alternative reading, on the terminal nothing won't appear because the vector size is 0.
+	for (size_t i = 0; i < v.size(); i++){
+		cout << "Numbers after with v.size() " << v[i] << endl;
+	}
+	
+}
