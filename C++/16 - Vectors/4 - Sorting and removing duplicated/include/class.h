4566444818511b5895349d15877211721e511a0e
Prox_y <30447649+Prox-y@users.noreply.github.com>
2021-11-29 11:12:35 +0100

    Add files via upload

---
+++ C++/16 - Vectors/4 - Sorting and removing duplicated/include/class.h
@@ -0,0 +1,20 @@
+#ifndef DE1E89B1_35CE_4801_8185_1F1526798462
+#define DE1E89B1_35CE_4801_8185_1F1526798462
+using vett = vector<double>;
+
+class vectors {
+	public:
+        static vett vector1;
+        static size_t size;
+        static int compare;
+
+        static void input();
+        static void ordinamento();
+        static void rimozione_duplicati();
+        static void output();
+
+	
+};
+
+
+#endif /* DE1E89B1_35CE_4801_8185_1F1526798462 */
