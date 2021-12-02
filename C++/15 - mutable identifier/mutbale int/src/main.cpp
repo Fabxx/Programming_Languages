5a4cb4aa486ac998ea4491bc54cfd658f2571286
Prox_y <30447649+Prox-y@users.noreply.github.com>
2021-12-02 13:02:50 +0100

    Update main.cpp

--- C++/15 - mutable identifier/mutbale int/src/main.cpp
+++ C++/15 - mutable identifier/mutbale int/src/main.cpp
@@ -36,7 +36,7 @@ int main() {
    cout << "First access:\n";
    cout<<access.a<<" "<<access.b<<"\n";
 	
-    //t.a=30; //Error occurs because a can not be changed, because object is constant.
+    //access.a=30; //Error occurs because a can not be changed, because object is constant.
    access.b=100; //b still can be changed, because b is mutable, despite we're using a const access. 
   
    cout << "Second Access:\n";
