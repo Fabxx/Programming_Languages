4e0853051a8b969c37e3affa58700c8cd401aa4e
-_ <30447649+Prox-y@users.noreply.github.com>
2021-12-15 05:08:07 -0500

    Delete C++/16 - Vectors/4 - Sorting and removing duplicated directory

--- C++/16 - Vectors/4 - Sorting and removing duplicated/include/libs.h
+++
@@ -1,8 +0,0 @@
-#ifndef A6BF566D_B7D1_45F8_BE99_E4C3C94A9D2C
-#define A6BF566D_B7D1_45F8_BE99_E4C3C94A9D2C
-#include <iostream>
-using namespace std;
-#include <vector>
-#include <class.h>
-
-#endif /* A6BF566D_B7D1_45F8_BE99_E4C3C94A9D2C */
