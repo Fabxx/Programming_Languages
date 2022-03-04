744179a639429149b2d0204b319d5f75a0f6f838
revix <30447649+revix-0@users.noreply.github.com>
2022-03-04 13:18:49 +0100

    Update class.cpp

--- C++/12 - Object Oriented Programming/Basic OOP with object/src/class.cpp
+++ C++/12 - Object Oriented Programming/Basic OOP with object/src/class.cpp
@@ -1,8 +1,6 @@
 #include "class.h"
 using namespace std;
 
-//object declaration on first class, if we need to access members from other classes, the objects must be referred to the desitnation classes we want to reach. 
- test obj;
 
 //NOTE: members AND variables of the same class don't need an object to be called. those can be called directly
 
@@ -16,7 +14,9 @@ void test::print2() {
 
 
 void test2::add() {
-
+ //object declaration on first class, if we need to access members from other classes, the objects must be referred to the desitnation classes we want to reach. 
+ //objects must be local to the function where we need to use it, or else we get a multiple declaration error.
+ test obj;
   //calling first class members with object based on it.
    obj.print1();
    obj.print2();
