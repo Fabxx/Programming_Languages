44db9268666b078785f1de832efbdd815f459657
revix <30447649+revix-0@users.noreply.github.com>
2022-04-26 21:24:09 +0200

    Update main.cpp

--- C++/30 - Class constructor/basic example/main.cpp
+++ C++/30 - Class constructor/basic example/main.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 /*A constructor is a basic function, inside a class, it takes the type "first", normally we would do simple "void sum()" for example
   and that sum() is a constructor, but with type "void" instead. A constructor in a class can be useful if a class has to do 
-  specific things that needs to be done only when needed, without creating overhead with more functions. */
+  specific things that needs to be done only when needed, without creating overhead with more functions. 
+  A class constructor can also maintain data at runtime when needed, so the data is stored in memory and doesn't have to be 
+  re-fetched everytime*/
 
 class first {
     private:
