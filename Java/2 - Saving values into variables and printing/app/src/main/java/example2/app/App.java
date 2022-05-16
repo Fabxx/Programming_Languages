68aa436f2b795761c7d52b16570942466199913e
SSUPII <42090377+SSUPII@users.noreply.github.com>
2022-05-16 12:50:05 +0200

    New Java Example: Printing variables

---
+++ Java/2 - Saving values into variables and printing/app/src/main/java/example2/app/App.java
@@ -0,0 +1,13 @@
+package example2.app;
+
+public class App {
+    public static void main(String[] args) {
+		int age = 20;
+		int beans = 9;
+		String profession = "A coffee machine.";
+
+		System.out.println("I am "+age+" years old");
+		System.out.println("I am "+profession+" and I got "+beans+" coffee beans ready to grind");
+
+    }
+}
