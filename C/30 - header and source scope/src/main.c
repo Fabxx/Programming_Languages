fb716c3c5c30c3173354c6f28da124115bef3b9c
revix <30447649+revix-0@users.noreply.github.com>
2022-06-20 18:20:08 +0200

    Update main.c

--- C/30 - header and source scope/src/main.c
+++ C/30 - header and source scope/src/main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include "vars.h"
 
-array //type is not recognized globally. Means that you'll have to create a totally new instance of the variable if it
-	  //has to be global. Instead, if a variable from .h is called in a funciton with a memory range in it's source,
-	  //the compiler can recognize it's type. Each global declaration has to be unique to avoid conflicts.
+array     //type is not recognized globally from the .h because in this way we're trying to create a new variable with the same name.
+	  //Instead, if a variable from .h is called in a funciton, the compiler will see the variable in the .h and not a new instance.
 
 int main()
 {
