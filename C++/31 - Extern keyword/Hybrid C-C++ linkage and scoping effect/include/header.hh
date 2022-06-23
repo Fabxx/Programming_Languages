d05f0fb4fc2b51de9910d7f774b2879a3bed502e
revix <30447649+revix-0@users.noreply.github.com>
2022-06-23 11:57:16 +0200

    Update header.hh

--- C++/31 - Extern keyword/Hybrid C-C++ linkage and scoping effect/include/header.hh
+++ C++/31 - Extern keyword/Hybrid C-C++ linkage and scoping effect/include/header.hh
@@ -3,7 +3,7 @@
 
 extern void print(); //extern makes scope of print global.
 
-/*THis is a function that will be putted in a .c file. The compiler will enable the linkage between cpp and .c files 
+/*This is a function that will be put in a .c file. The compiler will enable the linkage between cpp and .c files 
 to be able to write hybrid code. THis function with extern "C" will be treated as C code. 
  If you want the prototypes in the .h file to refer to C symbol names, then they must have extern "C" 
  when being interpreted as C++, and they should not have extern "C" when being interpreted as C --
