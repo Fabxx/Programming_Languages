2ba2fa14bddde9d7ccc045ad5c05b0ac410c1a83
Fabx <30447649+Fabxx@users.noreply.github.com>
2022-07-20 00:06:48 +0200

    Update main.c

--- C/14 - Parameters pass/pass by ref vs value/main.c
+++ C/14 - Parameters pass/pass by ref vs value/main.c
@@ -11,9 +11,8 @@ void passByVal(int x) {
     printf("Value of x from passByVal: %d\n", x);
 }
 
-// adding the ampersand in front of the variable makes it pass by reference
-// pass by reference essentially means passing the actual variable rather
-// than just the VALUE of the variable. 
+/* adding the ampersand in front of the variable makes it pass by reference
+** here we are passint the memory address of x ad a parameter*/
 void passByRef(int *x) {
     *x = 33;
     printf("Value of x from passByRef: %d\n", *x);
@@ -23,6 +22,10 @@ int main() {
     int x = 10;
     printf("Value of x from main: %d\n", x);
 
+    /*Without the & symbol, we won't make x a pointer and get the memory address of the previous X.
+    THe & symbol makes a variable a pointer, and we make it reference the old memory address of X, which was 
+    Filled in the above function. And as you can see below, we get the data from the previous memory address.
+    If we don't do these with pointers, we are COPYING the memory areas, resulting in redundant bytes in memory.*/
     passByVal(x);
     // will still be 10
     printf("Value of x, after passByVal: %d\n", x);
