42665482d1fdeeff2f378385b31fa77376c25e31
Fabx <30447649+Fabxx@users.noreply.github.com>
2022-07-20 10:27:04 +0200

    Update main.c

--- C/20 - pointers/malloc - comparison between pointers arrays/src/main.c
+++ C/20 - pointers/malloc - comparison between pointers arrays/src/main.c
@@ -19,10 +19,11 @@ char *inputstr();
 int main()
 {
 
-	char *stringptr1 = inputstr();
-	char *stringptr2 = inputstr();
+	char stringptr1, stringptr2;
+	
+	input(&stringptr1, &stringptr2);
 
-	if (strcmp(stringptr1, stringptr2) == 0) {
+	if (strcmp(&stringptr1, &stringptr2) == 0) {
 		puts("The strings are the same\n");
 	} else {
 		puts("the strings are different\n");
@@ -30,17 +31,17 @@ int main()
 	return (0);
 }
 
-char *inputstr() { 
-
-    char *string;
+char *inputstr(char *str, char *str2) { 
    
     //the malloc function allocates for one position n bytes of chars, depending how many we insert.
-    string = (char *) malloc(sizeof(char));
-
-    fgets(string, sizeof(string), stdin);
+    str = (char *) malloc(sizeof(char));
+    str2 = (char *) malloc(sizeof(char));
+   
+    fgets(str, sizeof(str), stdin);
+    fgets(str2, sizeof(str2), stdin);
     
     fflush(stdin);
-    return string;
-    free(string); //here we remove the mem allocation to make memory space.
+    free(str); //here we remove the mem allocation given.
+    free(str2);
 }
 
