988d6e924f223e983c9782b8210761c6cf9c28cc
Fabx <30447649+Fabxx@users.noreply.github.com>
2022-07-20 10:07:08 +0200

    Delete C/20 - pointers/pointer example directory

--- C/20 - pointers/pointer example/functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTIONS_H_INCLUDED
-#define FUNCTIONS_H_INCLUDED
-#include <stdio.h>
-#include <stdlib.h>
-
-//declaration of functions and libraries
-void pointer (int *z);
-
-#endif // FUNCTIONS_H_INCLUDED
