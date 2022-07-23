8f3c25f6ce5509077606889057ef740349b07f00
Fabx <30447649+Fabxx@users.noreply.github.com>
2022-07-23 12:36:45 +0200

    Update main.c

--- C/13 - Array usage, simple and complex/Array initialization with strings/src/main.c
+++ C/13 - Array usage, simple and complex/Array initialization with strings/src/main.c
@@ -3,10 +3,7 @@
 int main()
 {
 	/*we can initialize the array with multiple words instead of single chars with the pointer
-	NOTE: this can be done only when declaring the variable. So if you need to reference it in other source
-	files and make this a common array, you'll have to declare it as const char **words;
-	Then you initialize it in a source file, and reference it in the others once it's been initialized,
-	so the compiler will take the initialized values.*/
+	NOTE: this can be done only when declaring the variable.*/
 
 	const char *words[] = {"Word1", "Word2"};
 	printf("%s\n%s", words[0], words[1]);
