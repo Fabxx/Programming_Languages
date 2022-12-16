166689b01f6f11ca45d5e8bfcfc10715c39a4109
Fabx <30447649+Fabxx@users.noreply.github.com>
2022-12-16 15:12:21 +0100

    add second note for pointer allocation

--- C/20 - pointers/malloc with array of integers/src/main.c
+++ C/20 - pointers/malloc with array of integers/src/main.c
@@ -3,7 +3,10 @@
 
 /*NOTE: 16_T Indicates that we have an array of integer, that gets values of 16 bit lenght precision
  in the scanf we use %hd because we have to retreive a 16 bit value on the array
- on the printf we simply print the value contained in the size_t index i*/
+ on the printf we simply print the value contained in the size_t index i
+ 
+ NOTE 2: inside the malloc, sizeof(type) allows the pointer to have space only for one number of that type. If we have to store multiple bytes of that
+         number, we do sizeof(type)*N, where N is the number of elements we want to insert, and will give the correspondenting byte size to the pointer.*/
 
 int main()
 {
