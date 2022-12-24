cb58e558eb49cb245de9a160d52cc1eaa4b08729
Fabx <30447649+Fabxx@users.noreply.github.com>
2022-12-24 16:55:02 +0100

    Update main.c

--- C/20 - pointers/Accessing array elements with pointers/main.c
+++ C/20 - pointers/Accessing array elements with pointers/main.c
@@ -4,10 +4,14 @@
 
 /*Here we show an example about how to access to a specific array position by taking the element address
   with a array pointer, which aims to obtain the memory address of the element we assign to it in the
-  square brackets, this is useful for the files, especially if we want to write/read on a specific position*/
+  square brackets, this is useful for the files, especially if we want to write/read on a specific position
+  
+  NOTE: In function overloads, where these require a variable as a pointer, but you have a normal variable,
+        you can temporarily make the var a pointer inside the argument with & symbol. Like parameter pass.*/
 
 int main() {
-
+    
+  //equivalent of int *arr = (int*)malloc(sizeof(int)*MAX); which gives MAX positions.
     int arr[MAX];
 
     int i;
@@ -18,7 +22,8 @@ int main() {
             scanf("%d", &arr[i]);
 
         }
-
+    
+  //can also do = arr[3] 
     int *arrPtr = &arr[3];
 
     printf("Array element we searched:%d\n", *arrPtr);
