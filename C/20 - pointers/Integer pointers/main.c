80ac89e68cda62ef77ea11c83c2e07ba897a4023
Fabx <30447649+Fabxx@users.noreply.github.com>
2023-03-25 12:00:41 +0000

    Update main.c

--- C/20 - pointers/Integer pointers/main.c
+++ C/20 - pointers/Integer pointers/main.c
@@ -11,6 +11,8 @@
          ++*a would've done the right thing and is more efficient than 
          (*a)++ which unnecessarily has to return the old value stored at the address and 
          then increment it.
+         
+         See the pre-increment and post-increment logic in chapter 03 - For loops
  * 
  *       the values in the pointer are in hexadecimal (0-9 A-F), in printf %d converts the hex value
  *       into decimal, in this case a reaches 0xa which is "10".
@@ -29,13 +31,9 @@ int main()
   for (size_t i = 0; i < 10; ++i) {
     /*(*a)++; in this case the pointer is dereferenced and the value is returned, then increased.
               that's working but it's not efficient, instead if we don't have to do comparisons 
-              with the value itself, we can directly increase it without returning 
+              with the value itself before modification, we can directly increase it without returning 
               (remind to you the ++i and i++ difference, where changing the increment operator
-               order changes also the priority, so if we don't return the value of x with ++x we 
-               CAN'T do comparisons on the value. In this case the comparison works because
-               we are comparing the entire pointer with its value stored inside, which will
-               become 0xa and that's what we're looking for. But if we are looking for the 
-               explicit value that is not returned (aka 10) the comparison will never work
+               order changes also the priority of value return.
                
                The fist case can be useful only if we NEED the returned value before incrementing it
                to do something else.*/
