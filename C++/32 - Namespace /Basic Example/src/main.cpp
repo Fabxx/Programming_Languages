74d66c8c407e1b9ea507465527eb23ec41c58f79
Fabx <30447649+Fabxx@users.noreply.github.com>
2023-03-31 15:53:36 +0200

    Update main.cpp

--- C++/32 - Namespace /Basic Example/src/main.cpp
+++ C++/32 - Namespace /Basic Example/src/main.cpp
@@ -2,6 +2,8 @@
  * @file main.cpp
  * @author Fabx
  * @brief A namespace it's useful to distinguish functions that might have the same name, and avoids conflicts.
+ 	  NOTE: a namespace it's also useful to create new function, and call them without the need of a 
+	        constructor unlike classes, can also be called/integrated in classes.
  * @version 0.1
  * @date 2022-07-23
  * 
