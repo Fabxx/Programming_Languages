9fccea5d6df4bfd705e383ef16283f90b3a071b9
Fabx <30447649+Fabxx@users.noreply.github.com>
2023-11-22 09:55:42 +0100

    Update main.c

--- C/28 - enum type/basic example/main.c
+++ C/28 - enum type/basic example/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-/*Enumerators are variables where you can store numerical values for each of them. and can be used for 
-	comparison or be re-assigned an enumerator is directly accessible unlike a struct.
-	An enum can't be modified once setted.*/
+/*Enumerators are variables where you can store numerical values (ints and hex numbers) for each of them. and can be used for 
+	comparisons, and we can also assign these values to int vars if we need. an enumerator is directly accessible unlike a struct.
+	(so no enumerators enum object. The only case it is unaccessible is because of scope limits)
+ 	An enum can't be modified once set, like a const value.
+  
+  These can be useful when making documentation for specific codes, like ERROR_CODE documentation for example, where you write the number 
+  expected for ERROR_SUCCESS, ERROR_FAIL and so on. And will also make comparisons with these values much more understanable than just 
+  writing (if a == 707), instead we can write (if a == ERROR_FAIL (aka 707 number))
+  */
 
 enum enumerators
 {
