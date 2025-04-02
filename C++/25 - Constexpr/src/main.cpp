0f7fcb90edfacbd4076dc49fef452e296a0cfacd
Fabxx <30447649+Fabxx@users.noreply.github.com>
2025-04-02 11:38:05 +0200

    [C++] Update constexpr chapter

--- C++/25 - Constexpr/src/main.cpp
+++ C++/25 - Constexpr/src/main.cpp
@@ -24,6 +24,11 @@
                     -new, delete, delete[], throw, typeid and operator keywords.
                     -functions calls to non constexpr functions.
                     -function parameters, because their value it's known at runtime only.
+                
+                -Constexpr functions are implicitly inline.
+                
+                -Constexpr functions don't become extern when inline attribute is applied. unlike variables.
+                 See chapter 34 for reference.
 
             DIFFERENCES:
                     -Const means that an object cannot be modified at runtime.
@@ -43,10 +48,18 @@
 constexpr int func() {
     return 2*3;
 }
+// function call will be evaluated at compile time if arguments are also constexpr.
+constexpr int func2(int x, int y) {
+    return x-y;
+}
 
 int main(int argc, char *argv[])
 {
     constexpr int expression {7*4};
+
+    constexpr int x {5}, y {3};
+
+    func2(x, y);
 
     std::cout << expression << "\n";
 }
